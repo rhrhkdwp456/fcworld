ca132b4e62630fbb8f75a21a83f502ece5aa4215
rhrhkdwp456 <82999023+rhrhkdwp456@users.noreply.github.com>
2024-03-28 20:33:35 +0900

    코드 저장서 수정

--- server.js
+++ server.js
@@ -326,16 +326,35 @@ app.get('/edit/:id', async(요청, 응답)=>{
     응답.render('edit.ejs', { result : result })
 })
 
+app.get('/code/edit/:id', async(요청, 응답)=>{
+    let result = await db.collection('code').findOne({ _id : new ObjectId(요청.params.id) })  
+    응답.render('code-edit.ejs', { result : result })
+})
 
 app.put('/edit', async (요청, 응답) => {
     try{
-        let result = await db.collection('post').updateOne({ 
+        await db.collection('post').updateOne({ 
             _id : new ObjectId(요청.body.id),
             user : new ObjectId(요청.user._id) //본인이 쓴글인지 확인
         },
         {$set : { title : 요청.body.title, content : 요청.body.content}
         })
         응답.redirect('/list')
+    }catch(e){
+        console.log(e)
+        응답.status(404).send('이상한 url 입력함.') 
+    }
+})
+
+app.put('/code-edit', async (요청, 응답) => {
+    try{
+        await db.collection('code').updateOne({ 
+            _id : new ObjectId(요청.body.id),
+            user : new ObjectId(요청.user._id) //본인이 쓴글인지 확인
+        },
+        {$set : { title : 요청.body.title, content : 요청.body.content}
+        })
+        응답.redirect('/code-list')
     }catch(e){
         console.log(e)
         응답.status(404).send('이상한 url 입력함.') 
